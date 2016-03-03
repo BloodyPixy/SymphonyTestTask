d758082dc1d92ec63fcea2e45b4039623005593b
Taras Pasichnyk <dev.taras.pasichnyk@gmail.com>
2016-03-03 10:44:17 +0200

    Initial Commit

---
+++ SymphonyTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SymphonyTask
+//
+//  Created by Taras Pasichnyk on 3/3/16.
+//  Copyright © 2016 Taras Pasichnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SymphonyTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SymphonyTask
+//
+//  Created by Taras Pasichnyk on 3/3/16.
+//  Copyright © 2016 Taras Pasichnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
